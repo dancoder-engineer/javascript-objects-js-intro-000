bb13fc460099b0bf8dd52363dd83a1080eb35148
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-05 17:40:39 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,9 +1,9 @@
 var playlist = {"Meat Loaf": "Seize the Night"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  return Object.assign({}, playlist, { [artistName]: songTitle })
+  playlist.[artistName] = songTitle
 }
 
 
-playlist = updatePlaylist(playlist,'a','a')
+updatePlaylist(playlist,'a','a')
 console.log(playlist)
