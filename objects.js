44294f79de18a65b2cffbb4ed8a1ebb3a0df7021
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-05 17:42:01 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -1,10 +1,7 @@
 var playlist = {"Meat Loaf": "Seize the Night"}
 
 function updatePlaylist(playlist, artistName, songTitle) {
-  playlist[artistName] = songTitle
-}
+  playlist[artistName] = songTitle }
 
 function removeFromPlaylist(playlist, artistName) {
-  
-  
-}
+  delete playlist.artistName }
