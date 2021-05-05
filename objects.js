3d7cbc7019a895aba00eb4e47139947d8461f96f
DANIEL PAUL SEMINARA <danseminara@yahoo.com>
2021-05-05 17:39:28 +0000

    Automatically backed up by Learn

--- objects.js
+++ objects.js
@@ -3,3 +3,7 @@ var playlist = {"Meat Loaf": "Seize the Night"}
 function updatePlaylist(playlist, artistName, songTitle) {
   return Object.assign({}, playlist, { [artistName]: songTitle })
 }
+
+
+updatePlaylist(playlist,'a','a')
+console.log(playlist)
